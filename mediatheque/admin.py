15ec576697fc0fc453733e52a3eab389adba9fcd
Eléa De Sousa <bitsch.elea@gmail.com>
2026-02-03 13:51:33 +0100

    Création des modèles POO avec héritage pour Media

--- mediatheque/admin.py
+++ mediatheque/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Livre, DVD, CD, JeuPlateau, Membre, Emprunt
 
-# Register your models here.
+
+@admin.register(Livre)
+class LivreAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'isbn', 'disponible')
+    search_fields = ('titre', 'auteur', 'isbn')
+    list_filter = ('disponible',)
+
+
+@admin.register(DVD)
+class DVDAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'realisateur', 'duree', 'disponible')
+    search_fields = ('titre', 'realisateur')
+    list_filter = ('disponible',)
+
+
+@admin.register(CD)
+class CDAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'artiste', 'nombre_pistes', 'disponible')
+    search_fields = ('titre', 'artiste')
+    list_filter = ('disponible',)
+
+
+@admin.register(JeuPlateau)
+class JeuPlateauAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'editeur', 'nombre_joueurs_min', 'nombre_joueurs_max')
+    search_fields = ('titre', 'editeur')
+
+
+@admin.register(Membre)
+class MembreAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'email', 'date_inscription', 'actif')
+    search_fields = ('nom', 'prenom', 'email')
+    list_filter = ('actif',)
+
+
+@admin.register(Emprunt)
+class EmpruntAdmin(admin.ModelAdmin):
+    list_display = ('membre', 'get_media', 'date_emprunt', 'date_retour_prevue', 'date_retour_effective')
+    list_filter = ('date_emprunt', 'date_retour_effective')
+    search_fields = ('membre__nom', 'membre__prenom')
